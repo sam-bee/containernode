961ae02b73b9f22e98cc7f039eadcab1287c8124
sam-bee <130986804+sam-bee@users.noreply.github.com>
2026-03-16 05:16:54 +0000

    To provide tailnet database access, add a Flux-managed Postgres service

--- tailscale/run-tailscale-serve.sh
+++ tailscale/run-tailscale-serve.sh
@@ -2,4 +2,5 @@
 set -Eeuo pipefail
 
 tailscale serve --service=svc:jellyfin --https=443 http://127.0.0.1:8096
+tailscale serve --service=svc:postgres --tcp=5432 tcp://127.0.0.1:5432
 tailscale serve --service=svc:transmission --https=443 http://127.0.0.1:9091
